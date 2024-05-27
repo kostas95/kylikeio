eb793aa6aa0b997bdfc10740897531062a89091d
kostas95 <kostas9991@hotmail.com>
2024-05-27 22:01:10 +0300

    strdyb gmail migration

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -44,37 +44,82 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyAjWfZe57o4ex58xj5kdqkG5QaN8yHWc48',
-    appId: '1:677063786723:web:dfe6c80d9948e7684005d6',
-    messagingSenderId: '677063786723',
-    projectId: 'kylikeio-57a7d',
-    authDomain: 'kylikeio-57a7d.firebaseapp.com',
-    storageBucket: 'kylikeio-57a7d.appspot.com',
+    apiKey: 'AIzaSyDszvvlWMA_-Z3Da4rdBcuahu5LDDSWcno',
+    appId: '1:459425673243:web:d6d0bd2e916c020861c8be',
+    messagingSenderId: '459425673243',
+    projectId: 'kylikeio-ddb41',
+    authDomain: 'kylikeio-ddb41.firebaseapp.com',
+    storageBucket: 'kylikeio-ddb41.appspot.com',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAln6NsOuS29OOJsIcHECiBzcUzU0ZbsFg',
-    appId: '1:677063786723:android:25090de33bb7b3074005d6',
-    messagingSenderId: '677063786723',
-    projectId: 'kylikeio-57a7d',
-    storageBucket: 'kylikeio-57a7d.appspot.com',
+    apiKey: 'AIzaSyDe-L2yAFe4bbB52sOAUuIUgmGyBDP8WHw',
+    appId: '1:459425673243:android:d0be7f0e6f9d5b7e61c8be',
+    messagingSenderId: '459425673243',
+    projectId: 'kylikeio-ddb41',
+    storageBucket: 'kylikeio-ddb41.appspot.com',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyDa9IHYGOl_WlvobXr2hFuvFzFil2WbgWI',
-    appId: '1:677063786723:ios:c5651ecf400a43054005d6',
-    messagingSenderId: '677063786723',
-    projectId: 'kylikeio-57a7d',
-    storageBucket: 'kylikeio-57a7d.appspot.com',
+    apiKey: 'AIzaSyC99qTufoc81wThon2x1d7TE05OF2Ci-5A',
+    appId: '1:459425673243:ios:802c1c2b81835f6461c8be',
+    messagingSenderId: '459425673243',
+    projectId: 'kylikeio-ddb41',
+    storageBucket: 'kylikeio-ddb41.appspot.com',
     iosBundleId: 'com.example.kylikeio',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDa9IHYGOl_WlvobXr2hFuvFzFil2WbgWI',
-    appId: '1:677063786723:ios:de60f4049a2846974005d6',
-    messagingSenderId: '677063786723',
-    projectId: 'kylikeio-57a7d',
-    storageBucket: 'kylikeio-57a7d.appspot.com',
-    iosBundleId: 'com.example.kylikeio.RunnerTests',
+    apiKey: 'AIzaSyC99qTufoc81wThon2x1d7TE05OF2Ci-5A',
+    appId: '1:459425673243:ios:802c1c2b81835f6461c8be',
+    messagingSenderId: '459425673243',
+    projectId: 'kylikeio-ddb41',
+    storageBucket: 'kylikeio-ddb41.appspot.com',
+    iosBundleId: 'com.example.kylikeio',
   );
+
+  static const FirebaseOptions windows = FirebaseOptions(
+    apiKey: 'AIzaSyDszvvlWMA_-Z3Da4rdBcuahu5LDDSWcno',
+    appId: '1:459425673243:web:b50ab611d655ef7b61c8be',
+    messagingSenderId: '459425673243',
+    projectId: 'kylikeio-ddb41',
+    authDomain: 'kylikeio-ddb41.firebaseapp.com',
+    storageBucket: 'kylikeio-ddb41.appspot.com',
+  );
+
+  /// Old project data
+  // static const FirebaseOptions web = FirebaseOptions(
+  //   apiKey: 'AIzaSyAjWfZe57o4ex58xj5kdqkG5QaN8yHWc48',
+  //   appId: '1:677063786723:web:dfe6c80d9948e7684005d6',
+  //   messagingSenderId: '677063786723',
+  //   projectId: 'kylikeio-57a7d',
+  //   authDomain: 'kylikeio-57a7d.firebaseapp.com',
+  //   storageBucket: 'kylikeio-57a7d.appspot.com',
+  // );
+  //
+  // static const FirebaseOptions android = FirebaseOptions(
+  //   apiKey: 'AIzaSyAln6NsOuS29OOJsIcHECiBzcUzU0ZbsFg',
+  //   appId: '1:677063786723:android:25090de33bb7b3074005d6',
+  //   messagingSenderId: '677063786723',
+  //   projectId: 'kylikeio-57a7d',
+  //   storageBucket: 'kylikeio-57a7d.appspot.com',
+  // );
+  //
+  // static const FirebaseOptions ios = FirebaseOptions(
+  //   apiKey: 'AIzaSyDa9IHYGOl_WlvobXr2hFuvFzFil2WbgWI',
+  //   appId: '1:677063786723:ios:c5651ecf400a43054005d6',
+  //   messagingSenderId: '677063786723',
+  //   projectId: 'kylikeio-57a7d',
+  //   storageBucket: 'kylikeio-57a7d.appspot.com',
+  //   iosBundleId: 'com.example.kylikeio',
+  // );
+  //
+  // static const FirebaseOptions macos = FirebaseOptions(
+  //   apiKey: 'AIzaSyDa9IHYGOl_WlvobXr2hFuvFzFil2WbgWI',
+  //   appId: '1:677063786723:ios:de60f4049a2846974005d6',
+  //   messagingSenderId: '677063786723',
+  //   projectId: 'kylikeio-57a7d',
+  //   storageBucket: 'kylikeio-57a7d.appspot.com',
+  //   iosBundleId: 'com.example.kylikeio.RunnerTests',
+  // );
 }
